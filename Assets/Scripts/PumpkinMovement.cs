8bc3ecf40da39425a6df0e5a593b59a2f197eb1c
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-26 20:38:17 +0200

    PumpkinControls

--- Assets/Scripts/PumpkinMovement.cs
+++ Assets/Scripts/PumpkinMovement.cs
@@ -7,8 +7,14 @@ public class PumpkinMovement : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)]float moveSpeed = 10f;
     
-    void Update()
-    {
-        transform.Translate(moveSpeed * Time.deltaTime, 0f,0f);
+    void Update() {
+        MovePumpkin();
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp (pos.x, -8, 8);
+        pos.y = Mathf.Clamp (pos.y, -5, 1);
+        transform.position = pos;
+    }
+    void MovePumpkin() {
+        transform.Translate(moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"),0f);
     }
 }
